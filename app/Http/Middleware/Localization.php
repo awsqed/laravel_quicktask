712486d85c970e2657e3122bd97a7516e3177ee8
Tri Nguyen <xtringuyen1510@gmail.com>
2020-12-24 13:08:37 +0700

    Routing and create controllers

--- app/Http/Middleware/Localization.php
+++ app/Http/Middleware/Localization.php
@@ -3,22 +3,20 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Support\Facades\Auth;
 
-class RedirectIfAuthenticated
+class Localization
 {
     /**
      * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
-     * @param  string|null  $guard
      * @return mixed
      */
-    public function handle($request, Closure $next, $guard = null)
+    public function handle($request, Closure $next)
     {
-        if (Auth::guard($guard)->check()) {
-            return redirect('/task');
+        if (session()->has('locale')) {
+            \App::setLocale(session()->get('locale'));
         }
 
         return $next($request);
---
+++ resources/lang/vi/auth.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'login' => 'Đăng Nhập',
+    'register' => 'Đăng Ký',
+    'logout' => 'Đăng Xuất',
+    'email' => 'Email',
+    'password' => 'Mật Khẩu',
+    'remember' => 'Ghi Nhớ Tài Khoản',
+    'forgot' => 'Quên mật khẩu?',
+    'name' => 'Tên tài khoản',
+    'password.confirm' => 'Mật Khẩu (nhập lại)',
+    'reset.password' => 'Khôi Phục Mật Khẩu',
+    'send.password.reset.link' => 'Gửi đường dẫn khôi phục',
+    'failed' => 'Tên tài khoản hoặc mật khẩu không chính xác.',
+    'throttle' => 'Bạn đã đăng nhập sai quá nhiều lần. Vui lòng thử lại sau :seconds giây.',
+
+];
